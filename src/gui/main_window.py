9a306295a13f97a5cecf45b8b826f3a901717a53
nasser buhamad <nasser@be-free.app>
2026-02-11 06:54:40 +0300

    Working on implementig json data into gui

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,14 +1,15 @@
-
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, CENTER
+from toga.style.pack import COLUMN, CENTER, ROW
 from src.core.paths import IMAGES_PATH
+from src.services.candle_service import CandleService
 
 
 class MainWindow(toga.Box):
     def __init__(self):
         super().__init__(style=Pack(direction=COLUMN, text_align=CENTER, flex=1))
-        label = toga.Label("Welcome to Toga again", style=Pack(align_items=CENTER, flex=0.1))
+        label = toga.Label("Welcome to Toga again",
+                           style=Pack(align_items=CENTER, flex=0.1))
         image = toga.Image(IMAGES_PATH / "reversial" / "hammer.jpg")
         rimage = toga.Image(IMAGES_PATH / "reversial" / "hammer_example.jpg")
         image_view = toga.ImageView(image)
@@ -16,3 +17,16 @@ class MainWindow(toga.Box):
         self.add(label)
         self.add(image_view)
         self.add(rimage_view)
+        service = CandleService()
+        candles = service.get_bullish_candles()
+        labels = []
+        for candle in candles:
+            labels.append(toga.Label(candle['name_ar']))
+
+        print(labels)
+        box = toga.Box(direction=COLUMN)
+
+        for label in labels:
+            box.add(label)
+
+        self.add(box)
